06deec0f102d9526aa15852badbfeae62ccbb4cf
smd9788 <smd09876@gmail.com>
2019-03-15 14:44:21 -0400

    set apiconfig to new heroku app

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://battlegame-smd9788.herokuapp.com',
+  production: 'https://reactrcade-api.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
